e0df60f783aab21e51fc2cfc8603aa47e5cbac23
NuxCasino <rashedul.bcc@gmail.com>
2026-02-04 15:07:04 +0600

    update

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -1,12 +1,13 @@
 import createClient from 'openapi-fetch';
 import type { paths } from '@/lib/openapi';
+import { API_URL, API_KEY } from './config';
 
 export type ApiPaths = paths; // 👈 re-export type
 
 export const apiClient = createClient<paths>({
-  baseUrl: 'http://api.tirbet.online',
+  baseUrl: API_URL,
   headers: {
-    'x-api-key': "hello",
+    'x-api-key': API_KEY,
   },
 });
 
